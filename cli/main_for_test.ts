10497b7367c20820637943c732588f271a5c2503
Heejoon Kang <haseo.hackers@gmail.com>
2025-04-20 18:06:58 +0900

    tweak `main_for_tests.ts`

--- cli/main_for_test.ts
+++ cli/main_for_test.ts
@@ -2,4 +2,4 @@ import { findRootIndexFile, parseRootIndexFile } from "../mod.ts";
 
 const rootIndexFile = await findRootIndexFile(Deno.args[0]);
 const report = await parseRootIndexFile(rootIndexFile.absolutePath);
-console.log(report);
+console.log(JSON.stringify(report));
